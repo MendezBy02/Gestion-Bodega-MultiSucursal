56331d21094ea3cc916c16604148f0999f5ad79e
Abel Antonio Escobar Méndez <antonio.mendez1819@gmail.com>
2026-05-17 16:35:20 -0600

    Delete frontend directory

--- frontend/js/supabase.js
+++
@@ -1,8 +0,0 @@
-const supabaseUrl = "https://qldarbwshdoghxwnjmye.supabase.co";
-
-const supabaseKey = "sb_publishable_i8ds47Xrje7E9L1XiHpI2g_IW480x8Q";
-
-const db = window.supabase.createClient(
-  supabaseUrl,
-  supabaseKey
-);
