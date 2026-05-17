5b28ef49e375487c7290f456ea2349bd5cf7cb3e
Abel Antonio Escobar Méndez <antonio.mendez1819@gmail.com>
2026-05-17 15:10:07 -0600

    Update login-email.js

--- frontend/api/login-email.js
+++ frontend/api/login-email.js
@@ -40,7 +40,7 @@ export default async function handler(req, res){
         'NexoTech <onboarding@resend.dev>',
 
       to:
-        correo,
+         "antonio.escobarby02@gmail.com",,
 
       subject:
         'Inicio de sesión detectado',
