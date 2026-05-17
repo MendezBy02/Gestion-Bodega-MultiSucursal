6fb796016a954b5b922a2db71275d3f8ebd2c18f
Abel Antonio Escobar Méndez <antonio.mendez1819@gmail.com>
2026-05-17 16:36:01 -0600

    Add files via upload

---
+++ frontend/js/app.js
@@ -0,0 +1,240 @@
+// =========================
+// ELEMENTOS
+// =========================
+
+const toggleBtn =
+  document.getElementById("toggleBtn");
+
+const sidebar =
+  document.getElementById("sidebar");
+
+const layout =
+  document.getElementById("layout");
+
+// =========================
+// SESIÓN
+// =========================
+
+const user = JSON.parse(
+  localStorage.getItem("user")
+);
+
+// si no existe sesión
+if (!user) {
+
+  // bloquear historial
+  window.location.replace(
+    "index.html"
+  );
+
+}
+
+// mostrar nombre
+document.getElementById(
+  "nombreUsuario"
+).innerText = user.nombre;
+
+// =========================
+// CARGAR SUCURSAL
+// =========================
+
+async function cargarSucursal() {
+
+  const { data } = await db
+    .from("sucursal")
+    .select("nombre")
+    .eq("idsucursal", user.sucursal)
+    .single();
+
+  if (data) {
+
+    document.getElementById(
+      "sucursalUsuario"
+    ).innerText = data.nombre;
+
+  }
+
+}
+
+cargarSucursal();
+
+// =========================
+// TOGGLE SIDEBAR
+// =========================
+
+toggleBtn.addEventListener("click", () => {
+
+  const isMobile =
+    window.innerWidth <= 768;
+
+  // móvil
+  if (isMobile) {
+
+    sidebar.classList.toggle("active");
+
+    layout.classList.toggle("active");
+
+  } else {
+
+    // pc
+    layout.classList.toggle("collapsed");
+
+  }
+
+  // cambiar icono
+  toggleBtn.innerHTML =
+
+    layout.classList.contains("collapsed") ||
+    sidebar.classList.contains("active")
+
+      ? '<i data-lucide="x"></i>'
+      : '<i data-lucide="menu"></i>';
+
+  lucide.createIcons();
+
+});
+
+// =========================
+// CAMBIAR VISTAS
+// =========================
+
+function mostrar(id) {
+
+  const usuario =
+  JSON.parse(
+    localStorage.getItem("user")
+  );
+
+if(
+  id === "usuarios" &&
+  usuario.rol !== "admin"
+){
+
+  alert(
+    "No tienes permisos"
+  );
+
+  return;
+}
+
+  // ocultar vistas
+  document.querySelectorAll(".view")
+    .forEach(v => {
+      v.style.display = "none";
+    });
+
+  // mostrar vista
+  document.getElementById(id)
+    .style.display = "block";
+
+if(id === "dashboard"){
+
+  actualizarDashboard();
+
+}
+  // cerrar móvil
+  if (window.innerWidth <= 768) {
+
+    sidebar.classList.remove("active");
+
+    layout.classList.remove("active");
+
+    toggleBtn.innerHTML =
+      '<i data-lucide="menu"></i>';
+
+    lucide.createIcons();
+
+  }
+
+}
+
+// =========================
+// LOGOUT
+// =========================
+
+function logout() {
+
+  localStorage.removeItem("user");
+
+  window.location.replace(
+  "index.html"
+);
+
+}
+
+// =========================
+// CONTROL DE ROLES
+// =========================
+
+const usuarioSesion =
+  JSON.parse(
+    localStorage.getItem("user")
+  );
+
+if(usuarioSesion){
+
+  // elementos
+  const menuUsuarios =
+    document.getElementById(
+      "menuUsuarios"
+    );
+
+  const btnExportPDF =
+    document.getElementById(
+      "btnExportPDF"
+    );
+
+
+  // ======================
+  // ADMIN
+  // ======================
+
+  if(
+    usuarioSesion.rol ===
+    "admin"
+  ){
+
+    // menú usuarios
+    if(menuUsuarios){
+
+      menuUsuarios.style.display =
+        "block";
+
+    }
+
+    // botón exportar
+    if(btnExportPDF){
+
+      btnExportPDF.style.display =
+        "flex";
+
+    }
+
+  }
+
+
+  // ======================
+  // GERENTE
+  // ======================
+
+  else{
+
+    // ocultar usuarios
+    if(menuUsuarios){
+
+      menuUsuarios.style.display =
+        "none";
+
+    }
+
+    // ocultar pdf
+    if(btnExportPDF){
+
+      btnExportPDF.style.display =
+        "none";
+
+    }
+
+  }
+
+}
---
+++ frontend/js/login.js
@@ -0,0 +1,129 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  // formulario
+  const form = document.getElementById("loginForm");
+
+  // password
+  const pass = document.getElementById("password");
+
+  // toggle password
+  document.addEventListener("click", (e) => {
+
+    const btn = e.target.closest("#togglePass");
+
+    if (!btn) return;
+
+    if (pass.type === "password") {
+
+      pass.type = "text";
+
+      btn.setAttribute(
+        "data-lucide",
+        "eye-off"
+      );
+
+    } else {
+
+      pass.type = "password";
+
+      btn.setAttribute(
+        "data-lucide",
+        "eye"
+      );
+
+    }
+
+    lucide.createIcons();
+
+  });
+
+  // login
+  form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    // inputs
+    const email =
+      document.getElementById("email").value;
+
+    const password =
+      document.getElementById("password").value;
+
+    let valid = true;
+
+    // validar email
+    if (!email) {
+
+      document
+        .getElementById("email")
+        .closest(".input-group")
+        .classList.add("error");
+
+      valid = false;
+    }
+
+    // validar password
+    if (!password) {
+
+      document
+        .getElementById("password")
+        .closest(".input-group")
+        .classList.add("error");
+
+      valid = false;
+    }
+
+    if (!valid) return;
+
+    // consulta supabase
+    const { data, error } = await db
+      .from("usuario")
+      .select(`
+        *,
+        rol(nombre)
+      `)
+      .eq("email", email)
+      .eq("password", password)
+      .single();
+
+    // credenciales incorrectas
+    if (error || !data) {
+
+      mostrarToast("Credenciales incorrectas");
+
+      return;
+    }
+
+    // guardar sesión
+const usuario = {
+
+  id: data.idusuario,
+
+  nombre: data.nombre,
+
+  email: data.email,
+
+  rol: data.rol.nombre,
+
+  sucursal: data.idsucursal
+
+};
+
+
+// verificar
+console.log(usuario);
+
+
+// guardar
+localStorage.setItem(
+  "user",
+  JSON.stringify(usuario)
+);
+
+    // redirección
+    
+    window.location.href = "dashboard.html";
+
+  });
+
+});
